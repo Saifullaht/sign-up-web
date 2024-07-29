0d162fea6229b97be63c1e0a03c425d9b77bcd1b
Saifullaht <mrsaifullah025@gmail.in>
2024-07-29 23:33:36 +0500

    update complete

---
+++ scripts/app.js
@@ -0,0 +1,23 @@
+import { auth,onAuthStateChanged } from "./firebase.js";
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        window.location.href = "./pages/dashboard.html"; 
+        
+    } else {
+        window.location.href = "./pages/login.html"; 
+
+    // console.log("User is signed out");
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
+
--- scripts/dashbord.js
+++ scripts/dashbord.js
@@ -1,5 +1,19 @@
-import { auth, signOut } from "./firebase.js"
+import { auth, signOut , onAuthStateChanged } from "./firebase.js"
 const btn = document.querySelector("#logout_btn");
+const useremail = document.querySelector("#user_email");
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        // console.log(user);
+        useremail.innerText = user.email
+        const uid = user.uid;
+    } else {
+        window.location.href = "./login.html"; 
+
+    // console.log("User is signed out");
+    }
+  });
+
 
 btn.addEventListener("click", async() => {
 try {
